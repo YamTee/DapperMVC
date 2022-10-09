ede240389d2722fd124f78ac658d6a449d0ccd32
Mohammed Thalha <mohammed.t@ibridgellc.com>
2022-10-09 20:06:33 +0530

    Added DataAccess

---
+++ DapperDataAccess/Data/ProductData.cs
@@ -0,0 +1,36 @@
+﻿using DapperDataAccess.DataAccess;
+using DapperModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDataAccess.Data;
+
+public interface IProductData
+{
+	Task<Product?> GetProduct(int productId);
+	Task<IEnumerable<Product>> GetProducts();
+}
+
+public class ProductData : IProductData
+{
+	public readonly IBaseDataAccess _db;
+
+	public ProductData(IBaseDataAccess db)
+	{
+		_db = db;
+	}
+
+	public Task<IEnumerable<Product>> GetProducts() =>
+		_db.LoadData<Product, dynamic>("GetAllProducts", new { });
+
+	public async Task<Product?> GetProduct(int productId)
+	{
+		var results = await _db.LoadData<Product, dynamic>(
+			"GetProductByID",
+			new { ProductID = productId });
+		return results.FirstOrDefault();
+	}
+}
---
+++ DapperDataAccess/DataAccess/BaseDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace DapperDataAccess.DataAccess;
+
+public interface IBaseDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task SaveData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+}
+
+public class BaseDataAccess : IBaseDataAccess
+{
+    private readonly IConfiguration _config;
+    public BaseDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(
+        string storedProcedure,
+        U parameters,
+        string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T, U>(
+        string storedProcedure,
+        U parameters,
+        string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
